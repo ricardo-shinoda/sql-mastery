05a3aa389e2d7eaa7359e61e2cd038351e6df888
ricardo-shinoda <ricardoshinoda@gmail.com>
2026-03-26 08:34:10 -0300

    FEAT: data infra done in Linux manjaro

--- scripts/select.sql
+++ scripts/select.sql
@@ -4,3 +4,4 @@ SELECT
     (SELECT COUNT(*) FROM users) as total_usuarios,
     (SELECT COUNT(*) FROM song) as total_musicas,
     (SELECT COUNT(*) FROM streaming_history) as total_reproducoes;
+    
